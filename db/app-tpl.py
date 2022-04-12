b344c02b422e8e5bcdd16fcbb014bff22eccd822
Yingqian Gu <yga93@sfu.ca>
2022-04-11 22:18:11 -0700

    database tpl modification

--- db/app-tpl.py
+++ db/app-tpl.py
@@ -106,11 +106,20 @@ def read_new():
     else:
         owner = ""
 
+    if request.args.get('artist') != None:
+        artist = urllib.parse.unquote_plus(request.args.get('artist'))
+    else:
+        artist = ""
+
     table_name = objtype.capitalize()+"-ZZ-REG-ID"
     table_id = objtype + "_id"
     table = dynamodb.Table(table_name)
     if objkey or objkey != "":
-        if owner or owner != "":
+        if (owner or owner != "") and (artist or artist != ""):
+            response = table.scan(
+                    FilterExpression=Attr('SongTitle').eq(objkey) & Attr('Owner').eq(owner) & Attr('Artist').eq(artist)
+                )
+        elif (owner or owner != ""):
             response = table.scan(
                     FilterExpression=Attr('SongTitle').eq(objkey) & Attr('Owner').eq(owner)
                 )
@@ -265,31 +274,6 @@ def delete():
     table_id = objtype + "_id"
     table = dynamodb.Table(table_name)
     response = table.delete_item(Key={table_id: objkey})
-    return response
-
-@bp.route('/delete_music', methods=['DELETE'])
-def delete_music():
-    headers = request.headers  # noqa: F841
-    # check header here
-    objtype = urllib.parse.unquote_plus(request.args.get('objtype'))
-    # objkey = urllib.parse.unquote_plus(request.args.get('objkey'))
-    if request.args.get('objkey') != None:
-        objkey = urllib.parse.unquote_plus(request.args.get('objkey'))
-    else:
-        objkey = "" 
-    if request.args.get('owner') != None:
-        owner = urllib.parse.unquote_plus(request.args.get('owner'))
-    else:
-        owner = ""
-    table_name = objtype.capitalize()+"-ZZ-REG-ID"
-    table_id = objtype + "_id"
-    table = dynamodb.Table(table_name)
-    # response = table.delete_item(Key={table_id: objkey})
-    if objkey or objkey != "":
-        if owner or owner != "":
-            response = table.scan(
-                    FilterExpression=Attr('SongTitle').eq(objkey) & Attr('Owner').eq(owner)
-                )
     return response
 
 
